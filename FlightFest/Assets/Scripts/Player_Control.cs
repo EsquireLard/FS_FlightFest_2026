190c00a7fb3d68ec866f5a29947f8ca4b3ead1fb
Zachary Brodahl <zacharybrodahl@gmail.com>
2026-03-01 01:23:33 -0500

    input actions added. fixed visual studio package missing. various missing object fixes for managers and ui

---
+++ FlightFest/Assets/Scripts/Player_Control.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Control : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Input_Throttle(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    void Input_Yaw(InputAction.CallbackContext context)
+    {
+
+    }
+
+    void Input_Pitch(InputAction.CallbackContext context)
+    {
+
+    }
+
+    void Input_Roll(InputAction.CallbackContext context)
+    {
+
+    }
+}
