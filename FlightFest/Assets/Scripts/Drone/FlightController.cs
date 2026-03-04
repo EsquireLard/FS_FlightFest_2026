9ea5dea41d43a46861fb56c7e36d91068daaaf3e
emryscv <emryscv@gmail.com>
2026-03-04 01:04:28 -0500

    feat/RotorDesaturation

--- FlightFest/Assets/Scripts/Drone/FlightController.cs
+++ FlightFest/Assets/Scripts/Drone/FlightController.cs
@@ -33,12 +33,10 @@ public class FlightController : MonoBehaviour
     float[] rcRates;
     float[] rates;
 
-
     //OutputRotor Thrusts
-    float f1;
-    float f2;
-    float f3;
-    float f4;
+    float[] motorMix;
+
+    float[][] motorMixMatrix;
 
     void Awake()
     {
@@ -48,7 +46,7 @@ public class FlightController : MonoBehaviour
 
     void Start()
     {
-        prevTime = new float[3] {Time.time, Time.time, Time.time};
+        prevTime = new float[3] { Time.time, Time.time, Time.time };
         prevError = new float[3] { 0.0f, 0.0f, 0.0f };
         cumulativeI = new float[3] { 0.0f, 0.0f, 0.0f };
 
@@ -60,12 +58,24 @@ public class FlightController : MonoBehaviour
         rcExpo = new float[3] { 0.1f, 0.1f, 0.1f };
         rcRates = new float[3] { 1.0f, 1.0f, 1.0f };
         rates = new float[3] { 0.7f, 0.7f, 0.7f };
+
+        motorMix = new float[4] { 0f, 0f, 0f, 0f };
+        motorMixMatrix = new float[4][] {
+            new float[3] {  1, -1, -1 },
+            new float[3] {  1,  1,  1 },
+            new float[3] { -1, -1,  1 },
+            new float[3] { -1,  1, -1 }
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-         throttle = controls.RCController.Throttle.ReadValue<float>();
+        // Debug.Log("Controls: " + controls);
+        // Debug.Log("RC Controller: " + controls.RCController);
+        // Debug.Log("Throttle: " + controls.RCController.Throttle.ReadValue<float>());
+
+        throttle = controls.RCController.Throttle.ReadValue<float>();
         yaw = controls.RCController.Yaw.ReadValue<float>();
         pitch = controls.RCController.Pitch.ReadValue<float>();
         roll = controls.RCController.Roll.ReadValue<float>();
@@ -75,6 +85,7 @@ public class FlightController : MonoBehaviour
         float yawSetpoint = ComputeBetaflightRates(0, yaw);
         float pitchSetpoint = ComputeBetaflightRates(1, pitch);
         float rollSetpoint = ComputeBetaflightRates(2, roll);
+
         Debug.Log("Set: Throttle " + throttleSetpoint + " Yaw " + yawSetpoint + " Pitch " + pitchSetpoint + " Roll " + rollSetpoint);
 
         float yawPID = PIDEquation(yawSetpoint, state.angularVelocity.y, 0) / 1000.0f;
@@ -83,15 +94,35 @@ public class FlightController : MonoBehaviour
 
         Debug.Log("PID: Yaw " + yawPID + " Pitch " + pitchPID + " Roll " + rollPID);
 
-        f1 = throttleSetpoint + pitchPID - yawPID - rollPID;
-        f2 = throttleSetpoint + pitchPID + yawPID + rollPID;
-        f3 = throttleSetpoint - pitchPID - yawPID + rollPID;
-        f4 = throttleSetpoint - pitchPID + yawPID - rollPID;
+        // motorMix[0] = throttleSetpoint + pitchPID - yawPID - rollPID;
+        // motorMix[1] = throttleSetpoint + pitchPID + yawPID + rollPID;
+        // motorMix[2] = throttleSetpoint - pitchPID - yawPID + rollPID;
+        // motorMix[3] = throttleSetpoint - pitchPID + yawPID - rollPID;
+        float motorMin = float.MaxValue;
+        float motorMax = float.MinValue;
 
-        Debug.Log("Thrusts: F1 " + f1 + " F2 " + f2 + " F3 " + f3 + " F4 " + f4);
+        for (int i = 0; i < 4; i++)
+        {
+            motorMix[i] = motorMixMatrix[i][0] * pitchPID + motorMixMatrix[i][1] * yawPID + motorMixMatrix[i][2] * rollPID;
+            
+            motorMin = System.Math.Min(motorMin, motorMix[i]);
+            motorMax = System.Math.Max(motorMax, motorMix[i]);
+        }
 
-        //TODO thrust clamp
+        float motorRange = motorMax - motorMin;
 
+        Debug.Log("Thrusts: F1 " + motorMix[0] + " F2 " + motorMix[1] + " F3 " + motorMix[2] + " F4 " + motorMix[3] + " Min: " + motorMin + " Max: " + motorMax);
+        //Debug.Log("Min: " + motorMin + " Max: " + motorMax);
+        
+        float normalizationFactor = motorRange > 1.0f ? 1.0f / motorRange : 1.0f;
+        throttleSetpoint = Mathf.Clamp(throttleSetpoint, -motorMin * normalizationFactor, 1.0f - motorMax * normalizationFactor); 
+        
+        for (int i = 0; i < 4; i++)
+        {
+            motorMix[i] = throttleSetpoint + motorMix[i] * normalizationFactor;
+        }
+
+        Debug.Log("Normalized Thrusts: F1 " + (motorMix[0]) + " F2 " + (motorMix[1]) + " F3 " + (motorMix[2]) + " F4 " + (motorMix[3]) + " Throttle Setpoint: " + throttleSetpoint + " Normalization Factor: " + normalizationFactor);
     }
 
     float ComputeBetaflightRates(int axis, float input)
@@ -103,17 +134,9 @@ public class FlightController : MonoBehaviour
         float angleRate = 200.0f * rcRates[axis] * input;
 
         float rcSuperfactor = 1.0f - (inputAbs * rates[axis]);
-
-        if (rcSuperfactor < 0.01f)
-        {
-            rcSuperfactor = 0.01f;
-        }
-        else if (rcSuperfactor > 1.00f)
-        {
-            rcSuperfactor = 1.00f;
-        }
-
+        rcSuperfactor = Mathf.Clamp(rcSuperfactor, 0.01f, 1.00f);
         rcSuperfactor = 1.0f / rcSuperfactor;
+
         angleRate *= rcSuperfactor;
 
         return angleRate;
@@ -121,9 +144,9 @@ public class FlightController : MonoBehaviour
 
     float PIDEquation(float setpoint, float measurements, int axis)
     {
-        float deltaTime = Time.time - prevTime[axis]; //TODO This is not correct, we need to use the correct previous time for each axis
-        prevTime[axis] = Time.time;   
-        
+        float deltaTime = Time.time - prevTime[axis];
+        prevTime[axis] = Time.time;
+
         float error = setpoint - measurements;
         prevError[axis] = error;
 
@@ -131,10 +154,10 @@ public class FlightController : MonoBehaviour
         float P = kP[axis] * error;
 
         //Integral term
-        float I = cumulativeI[axis] + kI[axis] * error * deltaTime; //TODO This is not correct, we need to integrate the error over time
+        float I = cumulativeI[axis] + kI[axis] * error * deltaTime;
         cumulativeI[axis] = I;
 
-        if (I > 400) //TODO This is a very arbitrary threshold, we need to tune this
+        if (I > 400) //Betaflight values
         {
             I = 400;
         }
@@ -145,11 +168,11 @@ public class FlightController : MonoBehaviour
 
         //Derivative term
 
-        float D = kD[axis] * (error - prevError[axis]) / deltaTime; //TODO This is not correct, we need to divide by the time step
+        float D = kD[axis] * (error - prevError[axis]) / deltaTime;
 
         float PID = P + I + D;
 
-        if (PID > 500) //TODO This is a very arbitrary threshold, we need to tune this
+        if (PID > 500) //Betaflight values
         {
             PID = 500;
         }
@@ -162,16 +185,16 @@ public class FlightController : MonoBehaviour
     }
 }
 
-struct PIDresult
-{
-    public float PID;
-    public float cumulativeI;
-    public float prevError;
+// struct PIDresult
+// {
+//     public float PID;
+//     public float cumulativeI;
+//     public float prevError;
 
-    public PIDresult(float PID, float cumulativeI, float prevError)
-    {
-        this.PID = PID;
-        this.cumulativeI = cumulativeI;
-        this.prevError = prevError;
-    }
-}
+//     public PIDresult(float PID, float cumulativeI, float prevError)
+//     {
+//         this.PID = PID;
+//         this.cumulativeI = cumulativeI;
+//         this.prevError = prevError;
+//     }
+// }
